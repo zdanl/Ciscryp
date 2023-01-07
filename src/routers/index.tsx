70f6a87465660467e7f0ca4d8ff8fe4ca57cf92e
Daniel Zulla <dan@lyragit.com>
2023-01-07 11:33:31 +0000

    basename to /

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -48,7 +48,7 @@ export const pages: Page[] = [
 const MyRoutes = () => {
   return (
     <BrowserRouter
-      basename={process.env.NODE_ENV === "production" ? "" : ""}
+      basename={process.env.NODE_ENV === "production" ? "/" : "/"}
     >
       <ScrollToTop />
       <SiteHeader />
